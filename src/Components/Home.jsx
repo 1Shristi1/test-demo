84b79360003c5d146cb93bf97b8b67c3bcec9ce7
1Shristi1 <shristi.agarwal.in@rupeek.com>
2023-12-12 17:20:11 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { createUseStyles } from "react-jss";
+
+const useStyle = createUseStyles({
+  // upper: {
+  //   width: '100%',
+  // },
+  innerConatiner: {
+    borderRadius: '16px',
+    backgroundColor: "black",
+    display: "flex",
+    justifyContent: "space-between",
+    width: '100%',
+    color: "white",
+    padding: "10px",
+    overflow: 'hidden'
+  },
+});
+
+const Home = () => {
+  const classes = useStyle();
+  return (
+      <div className={classes.innerConatiner}>
+
+
+        <div>
+          Random Text
+        </div>
+        <div>
+          <img src="https://rupeek-website-assets.s3.ap-south-1.amazonaws.com/landing-pages/cards/goldReward1gm.png" alt="Image" width="100%" height="auto" />
+        </div>
+      </div>
+  )
+
+}
+
+export default Home
