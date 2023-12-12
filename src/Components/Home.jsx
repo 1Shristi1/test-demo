8c674dc1fad142c02001623b9f7af2dfe884078d
1Shristi1 <shristi.agarwal.in@rupeek.com>
2023-12-12 18:02:12 +0530

    added image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,34 +2,57 @@ import React from 'react'
 import { createUseStyles } from "react-jss";
 
 const useStyle = createUseStyles({
-  // upper: {
-  //   width: '100%',
-  // },
-  innerConatiner: {
+  wrapper:{
     borderRadius: '16px',
-    backgroundColor: "black",
-    display: "flex",
-    justifyContent: "space-between",
+    backgroundColor: "#262626",
+    padding: '24px 16px',
     width: '100%',
     color: "white",
-    padding: "10px",
-    overflow: 'hidden'
+    backgroundImage: "url(couponBackground.png)",
+    backgroundSize: 'cover',
+    display:'flex',
+    justifyContent: 'space-between',
+  },
+  innerConatiner: {
+  
+    display: "flex",
+    flexDirection: "column",
+    gap: '8px',
+  },
+  text: {
+    fontFamily: '"Inter- Medium", Helvetica',
+    fontSize: "12px",
+    fontWeight: "500",
+    lineHeight: "18px",
+  },
+  header: {
+fontFamily: '"Inter-Semibold", Helvetica',
+fontSize: "14px",
+fontWeight: "600",
+lineHeight: "22px",
+
   },
 });
 
 const Home = () => {
   const classes = useStyle();
   return (
+    <div className={classes.wrapper}>
       <div className={classes.innerConatiner}>
-
-
-        <div>
-          Random Text
-        </div>
-        <div>
-          <img src="https://rupeek-website-assets.s3.ap-south-1.amazonaws.com/landing-pages/cards/goldReward1gm.png" alt="something" width="100%" height="auto" />
-        </div>
+      <p className={classes.text}>
+        My reward
+      </p>
+      <p className={classes.header}>
+      50% off on Gold 
+Loan Interest
+      </p>
+      {/* <div className={classes.treasureImage}>  */}
+  
       </div>
+      <img src= "https://cdn-icons-png.flaticon.com/512/8913/8913819.png " alt= "gift" height="100px" width= "100px"/>
+
+     
+    </div>
   )
 
 }
